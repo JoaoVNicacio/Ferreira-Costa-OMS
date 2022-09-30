0e8604d65a1f2e1c7111e7c3846a4ac91e395665
JoaoVNicacio <joaonicks99@gmail.com>
2022-09-30 12:45:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  return (
+
+    <div className="App">
+
+      <section className='OrdersTable'>
+        <table>
+
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Status</th>
+            <th>Produtos</th>
+            <th>Data de entrega</th>
+          </tr>
+
+        </table>
+      </section>
+
+    </div>
+
+  )
+
+}
+
+export default App
