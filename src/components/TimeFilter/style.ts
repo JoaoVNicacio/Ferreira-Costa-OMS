e22864c67870fd0588fb5499dedeabd736339218
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-10-24 22:41:45 -0300

    Estilização do component de filtro de tempo

--- src/components/TimeFilter/style.ts
+++ src/components/TimeFilter/style.ts
@@ -4,6 +4,32 @@ export const TimeFilter = styled.div`
     width: 200px;
 
     .click{
-        background-color: #eee;
+        background-color: #fff;
+        padding: 5px;
+        /* font-size: ; */
+        font-family: "Inter", sans-serif;
+        border: none;
+        outline: none;
+        width: 200px;
+        /* color: ; */
+
+        &:hover{
+            background-color: #ddd;
+        }
+    }
+
+    .links{
+        padding: 5px;
+        /* font-size: ; */
+        font-family: "Inter", sans-serif;
+        border: none;
+        outline: none;
+        width: 200px;
+        /* color: ; */
+        background-color: lightblue; 
+
+        &:hover{
+            background-color: rgb(255,255,255);
+        }
     }
 `
