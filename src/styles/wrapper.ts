44039f239dd2cc58411113a3ca90accee87917c8
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-10-25 17:59:03 -0300

    Exclusão da terceira coluna do grid

--- src/styles/wrapper.ts
+++ src/styles/wrapper.ts
@@ -6,8 +6,9 @@ const Wrapper = styled.div`
     display: flex;
 
     display: grid;
-    grid-template-columns: 1fr 1fr 6fr;
+    grid-template-columns: 1fr 6fr;
     grid-template-rows: 15vw 80vw;
+    overflow-y: hidden;
 
     grid-template-areas: 
         "aside filter"
