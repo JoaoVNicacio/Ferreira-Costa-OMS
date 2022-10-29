d7aca0739a42b70dc01d7108fc39ac8e64389bbe
JoaoVNicacio <joaonicks99@gmail.com>
2022-10-29 13:44:40 -0300

    Adição de Fetch GET e mock com Array de objetos

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -101,4 +101,42 @@ const DashboardComponent = () => {
 
 }
 
+const getOrders = async () => {
+    fetch(`/orders`)
+
+        .then((response) => response.json())
+
+        .then((data) => {
+            console.log(data);
+
+            for (let i = 0; i <= data.length; i++) {
+
+            }
+
+        })
+}
+
+
+
+const mockedOrders : object = [
+    {
+        id: 2444,
+        name: "Johnson",
+        email: "johnson@email.com",
+        status: "Aguardando antifraude",
+        dateOfDeliveryDeadline: "20/10/2022 às 14:00",
+        dateOfEnter: "12/10/2022 às 14:00",
+        products: [
+            "1x Serra Circular", "1x Trena", "2x Caixa de ferramntas"
+        ],
+        spentTimeInAntifraud: "2 horas",
+        spentTimeInSeparation: "--",
+        spentTimeInPackaging: "--",
+        spentTimeInLogistics: "--",
+        spentTimeInTransportation: "--",
+        dateOfDelivery: "--",
+        commentary: ""
+    },
+]
+
 export default DashboardComponent
