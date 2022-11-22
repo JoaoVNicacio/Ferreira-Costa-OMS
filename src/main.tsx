da5b9f350ff8999a7a64e17d02629f867563e495
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-11-22 10:36:22 -0300

    Resolvendo o problema das rotas e deixando funcionais

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import GlobalStyles from './styles/global'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Login } from './pages/login'
+import { HomePage } from './pages/homePage'
+import { Pedidos } from './pages/pedidos'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles/>
+    <Router>
+            <Routes>
+                <Route path="/" element={<Login/>} />
+                <Route path="/homepage"  element={<HomePage/>}/>
+                <Route path="/pedidos"  element={<Pedidos/>}/>
+            </Routes>
+        </Router>
   </React.StrictMode>
 )
