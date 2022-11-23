17998757537b16ae532585334fe13d9522674715
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-11-23 08:56:03 -0300

    Adição da pasta do filtro

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { TimeFilter } from "./style"
+
+const Filter: any = ({ selected, setSelected }: any) => {
+
+    const [isActive, setIsActive] = useState(false);
+    const options = ['Este mês(Novembro)', 'Último mês(Outubro)', 'Último trimestre', 'Último semestre', 'Último ano']
+
+    return (
+        <TimeFilter>
+            <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>{selected}▼</div>
+            {isActive && (
+                <div className="dropdown-content">
+                    {options.map(option => (
+                        <div onClick={e => {
+                            setSelected(option)
+                            setIsActive(false)
+                        }
+                        } className="dropdown-item">
+                            {option}
+                        </div>
+                    ))}
+
+                </div>
+            )}
+        </TimeFilter>
+    )
+}
+
+export default Filter
---
+++ src/components/Filter/style.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const TimeFilter = styled.div`
+    width: 20vw;
+    height: 10vh;
+    padding: 1rem;
+    position: relative;
+    user-select: none;
+
+    .dropdown-btn{
+        padding: .5rem;
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        cursor: pointer;
+        border-radius: 8px;
+        box-shadow: 0 3px 3px 0px rgba(0, 0, 0,0.2);
+    }
+
+    .dropdown-content{
+        position: absolute;
+        padding: 5px;
+        background-color: #fff;
+        width: 35vh;
+        border-radius: 8px;
+        box-shadow: 0 3px 3px 0px rgba(0, 0, 0,0.2);
+    }
+
+    .dropdown-item{
+        padding: 5px;
+        cursor: pointer;
+        transition: all .2s;
+
+        &:hover{
+            background-color: #f4f4f4;
+            border-radius: 8px;
+        }
+    }
+`
