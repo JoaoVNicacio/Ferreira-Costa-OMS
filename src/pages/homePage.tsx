e103a2ec2aa705f59b3e82a1523124a6a59f8411
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-11-23 08:54:52 -0300

    Alterações nas importanções do filtro e adição do state da página de pedidos

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,7 +1,7 @@
 import SidebarComponent from "../components/Sidebar" 
 import * as W from '../styles/wrapper'
 import DashboardComponent from "../components/Dashboard"
-import Filter from "../components/TimeFilter"
+import Filter from "../components/Filter"
 import React, {useState} from 'react'
 
 export const HomePage = () => {
--- src/pages/pedidos.tsx
+++ src/pages/pedidos.tsx
@@ -1,13 +1,17 @@
+import React, {useState} from 'react'
 import SidebarComponent from "../components/Sidebar" 
-import Filter from "../components/TimeFilter"
+import Filter from "../components/Filter"
 import Table from '../components/Table'
 import * as W from '../styles/wrapper'
 
 export const Pedidos = () => {
+
+    const [selected, setSelected] = useState("")
+
     return (
         <W.WrapperHome>
             <SidebarComponent/>
-            <Filter/>
+            <Filter selected={selected} setSelected={setSelected}/>
             <Table/>
         </W.WrapperHome>
     )
