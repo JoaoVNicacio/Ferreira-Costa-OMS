ea5f56092488cb572b275f16b5771c9950feec4a
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-11-24 11:32:17 -0300

    Organização dos estilos do comonent de dashboard

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,185 +1,171 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.section`
-    .dashboard{
+    .dashboard {
         width: 98%;
         height: auto;
         margin: 0;
 
-        hr{
+        hr {
             color: #000;
             border: 0.3 rem;
         }
 
-        .rowOfItems{
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            width: 100%;
-        }
-
-        .upperItem{
-            
+        .upperItem {
             width: 99.5%;
             height: auto;
-            background-color: #FFF;
+            background-color: #fff;
             border-radius: 8px;
             margin: 0 0.7% 0.7% 0.7%;
-            box-shadow: 0 3px 3px 0px rgba(0, 0, 0,0.2);
+            box-shadow: 0 3px 3px 0px rgba(0, 0, 0, 0.2);
             padding: 0;
             display: flex;
             justify-content: center;
 
-            table{
+            table {
                 width: 48rem;
-        
-                tr{
+
+                tr {
                     width: 48rem;
                     display: flex;
                     justify-content: space-around;
 
-                    th{
-                        display: inline-block;
-                        margin: 0.5rem;
-                        width: 8rem;
-                        text-align: center;
-                    }
-        
-                    td{
+                    th {
                         display: inline-block;
                         margin: 0.5rem;
                         width: 8rem;
                         text-align: center;
                     }
 
-                }
-            }
-        }
-
-
-        .leftBiggerItem{
-            width: 75%;
-            height: 18rem;
-            background-color: #FFF;
-            border-radius: 8px;
-            margin: 0.7% 0.7% 0.7% 0.7%;
-            box-shadow: 0 3px 3px 0px rgba(0, 0, 0,0.2);
-            overflow-y: scroll;
-            display: flex;
-            justify-content: center;
-
-
-            table{
-
-                tr:first-child{
-
-                    th{
-                        width: 20rem;
-                        display: inline-block;
-                        margin: 0.5rem 2rem;
-                        text-align: center;
-                        font-size: 14px;
-                    }
-                }
-
-                tr{
-                    width: auto;
-                    display: flex;
-                    justify-content: center;
-
-                    th{
-                        width: 11rem;
-                        display: inline-block;
-                        margin: 0.5rem 0.2rem;
-                        text-align: center;
-                        font-size: 12px;
-                    }
-        
-                    td{
-                        width: 8rem;
+                    td {
                         display: inline-block;
                         margin: 0.5rem;
+                        width: 8rem;
                         text-align: center;
-                        font-size: 12px;
-                    }
-
-                    div{
-                        background-color: #F9F6F6;
-                        border: solid 1px;
-                        border-radius: 8px;
-                        box-shadow: 0 2px 2px 0px rgba(0, 0, 0,0.2);
-                        margin: 0.5%;
-                        width: 100%;
-                        display: flex;
-                        justify-content: space-between;
-                        
-        
                     }
                 }
             }
         }
 
-        .rightSmallerItem{
-            width: 38%;
-            height: 18rem;
-            background-color: #FFF;
-            border-radius: 8px;
-            margin: 0.7% 0.7% 0.7% 0.7%;
-            box-shadow: 0 3px 3px 0px rgba(0, 0, 0,0.2);
-            overflow-y: scroll;
+        .rowOfItems {
             display: flex;
+            flex-direction: row;
             justify-content: center;
+            width: 100%;
 
+            .leftBiggerItem {
+                width: 75%;
+                height: 18rem;
+                background-color: #fff;
+                border-radius: 8px;
+                margin: 0.7% 0.7% 0.7% 0.7%;
+                box-shadow: 0 3px 3px 0px rgba(0, 0, 0, 0.2);
+                overflow-y: scroll;
+                display: flex;
+                justify-content: center;
 
-            table{
+                table {
+                        tr:first-child {
+                                th {
+                                    width: 20rem;
+                                    display: inline-block;
+                                    margin: 0.5rem 2rem;
+                                    text-align: center;
+                                    font-size: 14px;
+                                }
+                        }
 
-                tr:first-child{
+                        tr {
+                            width: auto;
+                            display: flex;
+                            justify-content: center;
 
-                    th{
-                        width: 20rem;
-                        display: inline-block;
-                        margin: 0.5rem 2rem;
-                        text-align: center;
-                        font-size: 14px;
-                    }
+                            th {
+                                width: 11rem;
+                                display: inline-block;
+                                margin: 0.5rem 0.2rem;
+                                text-align: center;
+                                font-size: 12px;
+                            }
+
+                            td {
+                                width: 8rem;
+                                display: inline-block;
+                                margin: 0.5rem;
+                                text-align: center;
+                                font-size: 12px;
+                            }
+
+                            div {
+                                background-color: #f9f6f6;
+                                border: solid 1px;
+                                border-radius: 8px;
+                                box-shadow: 0 2px 2px 0px rgba(0, 0, 0, 0.2);
+                                margin: 0.5%;
+                                width: 100%;
+                                display: flex;
+                                justify-content: space-between;
+                            }
+                        }
                 }
+            }
 
-                tr{
-                    width: 22rem;
-                    display: flex;
-                    justify-content: center;
+            .rightSmallerItem {
+                width: 38%;
+                height: 18rem;
+                background-color: #fff;
+                border-radius: 8px;
+                margin: 0.7% 0.7% 0.7% 0.7%;
+                box-shadow: 0 3px 3px 0px rgba(0, 0, 0, 0.2);
+                overflow-y: scroll;
+                display: flex;
+                justify-content: center;
 
-                    th{
-                        width: 2.5rem;
-                        display: inline-block;
-                        margin: 0.3rem 2rem;
-                        text-align: center;
-                        font-size: 12px;
-                    }
-        
-                    td{
-                        width: 6rem;
-                        display: inline-block;
-                        margin: 0.5rem;
-                        text-align: center;
-                        font-size: 12px;
-                    }
+                table {
+                    tr:first-child {
+                        th {
+                            width: 20rem;
+                            display: inline-block;
+                            margin: 0.5rem 2rem;
+                            text-align: center;
+                            font-size: 14px;
+                        }
+                        }
 
-                    div{
-                        background-color: #F9F6F6;
-                        border: solid 1px;
-                        border-radius: 8px;
-                        box-shadow: 0 2px 2px 0px rgba(0, 0, 0,0.2);
-                        margin: 0.5%;
-                        width: 100%;
+                        tr {
+                        width: 22rem;
                         display: flex;
-                        justify-content: space-between;
-                        
-        
+                        justify-content: center;
+
+                        th {
+                            width: 2.5rem;
+                            display: inline-block;
+                            margin: 0.3rem 2rem;
+                            text-align: center;
+                            font-size: 12px;
+                        }
+
+                        td {
+                            width: 6rem;
+                            display: inline-block;
+                            margin: 0.5rem;
+                            text-align: center;
+                            font-size: 12px;
+                        }
+
+                        div {
+                            background-color: #f9f6f6;
+                            border: solid 1px;
+                            border-radius: 8px;
+                            box-shadow: 0 2px 2px 0px rgba(0, 0, 0, 0.2);
+                            margin: 0.5%;
+                            width: 100%;
+                            display: flex;
+                            justify-content: space-between;
+                        }
                     }
                 }
             }
         }
     }
-
-`
+`;
