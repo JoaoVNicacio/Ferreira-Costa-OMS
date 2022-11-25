33518a2ebbc91871c297ef0687d6e03d0aaf0af6
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-11-25 09:28:30 -0300

    Alteração nos estilos do dashboard para ficarem mais adaptativos

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -19,11 +19,10 @@ export const Container = styled.section`
             margin: 0 0.7% 0.7% 0.7%;
             box-shadow: 0 3px 3px 0px rgba(0, 0, 0, 0.2);
             padding: 0;
-            display: flex;
-            justify-content: center;
 
             table {
-                width: 48rem;
+                width: auto;
+                margin: 0 auto;
 
                 tr {
                     width: 48rem;
@@ -51,6 +50,7 @@ export const Container = styled.section`
             display: flex;
             flex-direction: row;
             justify-content: center;
+            margin: 0 0.7% 0.7% 0.7%;
             width: 100%;
 
             .leftBiggerItem {
@@ -58,55 +58,58 @@ export const Container = styled.section`
                 height: 18rem;
                 background-color: #fff;
                 border-radius: 8px;
-                margin: 0.7% 0.7% 0.7% 0.7%;
+                margin: 0.7% 0 0 0;
                 box-shadow: 0 3px 3px 0px rgba(0, 0, 0, 0.2);
-                overflow-y: scroll;
                 display: flex;
                 justify-content: center;
 
                 table {
-                        tr:first-child {
-                                th {
-                                    width: 20rem;
-                                    display: inline-block;
-                                    margin: 0.5rem 2rem;
-                                    text-align: center;
-                                    font-size: 14px;
-                                }
-                        }
-
-                        tr {
-                            width: auto;
-                            display: flex;
-                            justify-content: center;
-
+                    
+                    tr:first-child {
                             th {
-                                width: 11rem;
+                                width: 20rem;
                                 display: inline-block;
-                                margin: 0.5rem 0.2rem;
+                                margin: 0.5rem 2rem;
                                 text-align: center;
-                                font-size: 12px;
+                                font-size: 14px;
                             }
+                    }
 
-                            td {
-                                width: 8rem;
-                                display: inline-block;
-                                margin: 0.5rem;
-                                text-align: center;
-                                font-size: 12px;
-                            }
+                    tr {
+                        width: auto;
+                        display: flex;
+                        justify-content: center;
 
-                            div {
-                                background-color: #f9f6f6;
-                                border: solid 1px;
-                                border-radius: 8px;
-                                box-shadow: 0 2px 2px 0px rgba(0, 0, 0, 0.2);
-                                margin: 0.5%;
-                                width: 100%;
-                                display: flex;
-                                justify-content: space-between;
-                            }
+                        thead{
+                            
                         }
+
+                        th {
+                            width: 11rem;
+                            margin: 0.5rem 0.2rem;
+                            text-align: center;
+                            font-size: 12px;
+                        }
+
+                        td {
+                            width: 8rem;
+                            display: inline-block;
+                            margin: 0.5rem;
+                            text-align: center;
+                            font-size: 12px;
+                        }
+
+                        div {
+                            background-color: #f9f6f6;
+                            border: solid 1px;
+                            border-radius: 8px;
+                            box-shadow: 0 2px 2px 0px rgba(0, 0, 0, 0.2);
+                            margin: 0.5%;
+                            width: 100%;
+                            display: flex;
+                            justify-content: space-between;
+                        }
+                    }
                 }
             }
 
@@ -117,7 +120,7 @@ export const Container = styled.section`
                 border-radius: 8px;
                 margin: 0.7% 0.7% 0.7% 0.7%;
                 box-shadow: 0 3px 3px 0px rgba(0, 0, 0, 0.2);
-                overflow-y: scroll;
+
                 display: flex;
                 justify-content: center;
 
@@ -130,9 +133,9 @@ export const Container = styled.section`
                             text-align: center;
                             font-size: 14px;
                         }
-                        }
+                    }
 
-                        tr {
+                    tr {
                         width: 22rem;
                         display: flex;
                         justify-content: center;
